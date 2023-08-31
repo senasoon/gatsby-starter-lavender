11cf7a4a9b1bf1d6be24965313e6e2b68195d388
senasoon <senasoon0@gmail.com>
2023-09-01 02:19:40 +0900

    init

---
+++ blog-config.ts
@@ -0,0 +1,22 @@
+const config = {
+  title: 'senasoon blog', // Blog name
+  author: 'senasoon', // Your name
+  description: '빠르게 실패하기',
+  siteUrl: 'https://senasoon.netlify.app/',
+  thumbnail: '/thumbnails/main.png', // Default blog thumbnail. i.e. '/thumbnails/main.png'
+  social: {
+    github: 'senasoon', // GitHub username
+    twitter: '', // Twitter username
+    facebook: '', // Facebook username
+    linkedin: '', // LinkedIn username
+    instagram: '', // Instagram username
+  },
+  comment: {
+    utterances: 'https://github.com/senasoon', // Your GitHub repository
+    disqusShortName: '', // Disqus short name
+  },
+  icon: 'src/images/profile.png', // path of blog icon
+  googleAnalytics: '0', // Google Analytics tracking id
+};
+
+export default config;
